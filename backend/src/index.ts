63a8cde49e9aadff7f548b2e5fd0e1730aa3d2a8
Toni Pakarinen <toni.pakarinen@eng.tamk.fi>
2019-05-13 16:24:35 +0300

    update schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,5 @@
-// import { ApolloServer, gql } from 'apollo-server';
 import { GraphQLServer } from "graphql-yoga";
 import { permissions } from "./permissions";
-// import { prisma } from './generated/prisma-client';
 import * as jwt from "jsonwebtoken";
 import { JWT_SECRET } from "./environment";
 import { typeDefs, resolvers } from "./schema";
@@ -13,6 +11,7 @@ const server = new GraphQLServer({
   context: async ctx => {
     const auth = ctx.request.get("Authorization");
     let currentUser = {};
+    console.log(auth);
     try {
       currentUser = await jwt.verify(auth.replace("Bearer ", ""), JWT_SECRET);
     } catch (e) {
