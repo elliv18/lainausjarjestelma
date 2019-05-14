60b80337c2081357a1d254339749c784a7df7554
Eevert Ahola <eevert.ahola@eng.tamk.fi>
2019-05-14 15:08:20 +0300

    apollosydeemia

--- frontend/pages/users.js
+++ frontend/pages/users.js
@@ -1,10 +1,11 @@
 import App from '../components/App'
 import MiniDrawer from '../components/MiniDrawer'
+import Print from '../components/Print'
 
 export default() => (
     <App>
         <MiniDrawer>
-            
+            <Print/>
         </MiniDrawer>
     </App>
 )
