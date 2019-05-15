2a260e785a343594ce8b5728a44337c261ee0265
Eevert Ahola <eevert.ahola@eng.tamk.fi>
2019-05-15 13:12:19 +0300

    table muokkaus

--- frontend/components/Equipments.js
+++ frontend/components/Equipments.js
@@ -8,6 +8,7 @@ import {
   DataTypeProvider,
   SearchState,
   IntegratedFiltering,
+  EditingState
 } from '@devexpress/dx-react-grid';
 import {
   Grid,
@@ -15,6 +16,8 @@ import {
   TableHeaderRow,
   SearchPanel,
   Toolbar,
+  TableEditRow,
+  TableEditColumn,
 } from '@devexpress/dx-react-grid-material-ui';
 import Check from '@material-ui/icons/check'
 import Cancel from '@material-ui/icons/cancel'
@@ -24,93 +27,44 @@ import SaveIcon from '@material-ui/icons/Save';
 import { ApolloConsumer } from "react-apollo"
 
 const styles = theme => ({
-    root: {
-      width: '90%',
-      marginTop: theme.spacing.unit * 3,
-      overflowX: 'auto',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-    table: {
-      minWidth: 700,
-    },
-  });
+  root: {
+    width: '90%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
   
-  let id = 0;
-  function createData(
-    name, model, info, loanStatus) {
-    id += 1;
-    return { id, name, model, info, loanStatus };
-  }
-  
-  const rows = [
-    createData('Kannettava', 'Lenovo', 'Kiva kone', false),
-    createData('Laite 2', 'Asus', 'Hahaa', false),
-    createData('Tv', 'Lg', '120 tuumainen OLED', true),
-    createData('Laite 4', 'Abc', 'infoo!', false),
-    createData('Kone 5', 'Cds', 'infoo123123', false),
-  ];
 
-  const BooleanTypeProvider = props => (
-    <DataTypeProvider
-      formatterComponent={BooleanFormatter}
-      {...props}
-    />
-  );
+const BooleanTypeProvider = props => (
+  <DataTypeProvider
+    formatterComponent={BooleanFormatter}
+    {...props}
+  />
+);
 
-  const AddButton = ({ onExecute }) => (
-    <div style={{ textAlign: 'center' }}>
-      <Button
-        color="primary"
-        onClick={onExecute}
-        title="Create new row"
-      >
-        New
-      </Button>
-    </div>
-  );
-  
-  const EditButton = ({ onExecute }) => (
-    <IconButton onClick={onExecute} title="Edit row">
-      <EditIcon />
-    </IconButton>
-  );
-  
-  const DeleteButton = ({ onExecute }) => (
-    <IconButton
-      onClick={() => {
-        // eslint-disable-next-line
-        if (window.confirm('Are you sure you want to delete this row?')) {
-          onExecute();
-        }
-      }}
-      title="Delete row"
-    >
-      <DeleteIcon />
-    </IconButton>
-  );
-  
-  const CommitButton = ({ onExecute }) => (
-    <IconButton onClick={onExecute} title="Save changes">
-      <SaveIcon />
-    </IconButton>
-  );
-  
-  const CancelButton = ({ onExecute }) => (
-    <IconButton color="secondary" onClick={onExecute} title="Cancel changes">
-      <CancelIcon />
-    </IconButton>
-  );
+let id = 0;
+function createData(
+  name, model, info, loanStatus) {
+  id += 1;
+  return { id, name, model, info, loanStatus };
+}
 
-  const commandComponents = {
-    add: AddButton,
-    edit: EditButton,
-    delete: DeleteButton,
-    commit: CommitButton,
-    cancel: CancelButton,
-  };
+const rows = [
+  createData('Kannettava', 'Lenovo', 'Kiva kone', false),
+  createData('Laite 2', 'Asus', 'Hahaa', false),
+  createData('Tv', 'Lg', '120 tuumainen OLED', true),
+  createData('Laite 4', 'Abc', 'infoo!', false),
+  createData('Kone 5', 'Cds', 'infoo123123', false),
+];
 
-  const BooleanFormatter = ({ value }) => 
+const getRowId = row => row.id;
+
+const BooleanFormatter = ({ value }) => 
   <Chip color={value? 'primary' : 'secondary'} 
         label={value ? 'Loaned' : 'Available'} 
         icon={value ? <Cancel /> : <Check/>}
@@ -119,7 +73,7 @@ const styles = theme => ({
                         justifyContent: 'left'}
         :{backgroundColor: 'rgba(0,128,0,0.75)'}}
         />;
-
+  
 
 class Equipments extends React.Component{
   constructor(props) {
@@ -138,16 +92,39 @@ class Equipments extends React.Component{
         { columnName: 'name', wordWrapEnabled: true},
         { columnName: 'model', wordWrapEnabled: true},
         { columnName: 'info', wordWrapEnabled: true},
-        { columnName: 'loanStatus', wordWrapEnabled: true, width: 150 }
+        { columnName: 'loanStatus', wordWrapEnabled: true, width: 150 },
       ],
       booleanColumns: ['loanStatus'],
       rows: rows,
       sorting: [{ columnName: 'id', direction: 'asc' }],
       
     };
-
+    this.commitChanges = this.commitChanges.bind(this);
     this.changeSorting = sorting => this.setState({ sorting });
   }
+
+  commitChanges({ added, changed, deleted }) {
+    let { rows } = this.state;
+    if (added) {
+      const startingAddedId = rows.length > 0 ? rows[rows.length - 1].id + 1 : 0;
+      rows = [
+        ...rows,
+        ...added.map((row, index) => ({
+          id: startingAddedId + index,
+          ...row,
+        })),
+      ];
+    }
+    if (changed) {
+      rows = rows.map(row => (changed[row.id] ? { ...row, ...changed[row.id] } : row));
+    }
+    if (deleted) {
+      const deletedSet = new Set(deleted);
+      rows = rows.filter(row => !deletedSet.has(row.id));
+    }
+    this.setState({ rows });
+  }
+
     render(){
         const{classes} = this.props
         const { rows, columns, tableColumnExtensions, sorting, booleanColumns } = this.state;
@@ -156,8 +133,9 @@ class Equipments extends React.Component{
         <Grid
           rows={rows}
           columns={columns}
-          
+          getRowId={getRowId}
         >
+        <EditingState onCommitChanges={this.commitChanges}/>
         <SearchState />
         <IntegratedFiltering />
         <BooleanTypeProvider 
@@ -170,6 +148,12 @@ class Equipments extends React.Component{
           <IntegratedSorting />
           <Table columnExtensions={tableColumnExtensions} />
           <TableHeaderRow showSortingControls />
+          <TableEditRow />
+          <TableEditColumn
+            showAddCommand
+            showEditCommand
+            showDeleteCommand
+          />
           <Toolbar />
           <SearchPanel />
         </Grid>
