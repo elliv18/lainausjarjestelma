763c75ce04764175a3cdf1401ea8831e0ee8b634
Toni Pakarinen <toni.pakarinen@eng.tamk.fi>
2019-05-23 10:30:54 +0300

    adding eslint & prettier

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -27,9 +27,8 @@ import { CURRENTUSER } from '../lib/gql/queries';
 import Moment from 'react-moment';
 import 'moment-timezone';
 
-
 const styles = {
-  root:{
+  root: {
     width: '90%',
     marginLeft: 'auto',
     marginRight: 'auto',
@@ -44,11 +43,11 @@ const styles = {
     backgroundColor: 'lightGrey',
     padding: 10,
     marginLeft: 'auto',
-    marginRight:'auto',
+    marginRight: 'auto',
     marginBottom: '10px',
     marginTop: '10px',
   },
-  paperTable:{
+  paperTable: {
     width: '90%',
     marginLeft: 'auto',
     marginRight: 'auto',
@@ -56,7 +55,7 @@ const styles = {
   card: {
     textAlign: 'left',
   },
-}
+};
 
 /******************************** CLASS ******************************/
 class Home extends React.Component {
@@ -73,91 +72,99 @@ class Home extends React.Component {
         { name: 'returnDate', title: 'return date' },
         { name: 'dueDate', title: 'Due date' },
       ],
-      
+
       client: props.client,
       data_user: {},
-      data_loans: []
+      data_loans: [],
     };
     this.changeSorting = sorting => this.setState({ sorting });
   }
 
-  async componentDidMount(){
-    let temp = await this.state.client.query({ query: CURRENTUSER })
-    console.log(temp)
-    let temp_user
-    let temp_loans = []
-    if (temp.data.currentUser){
+  async componentDidMount() {
+    let temp = await this.state.client.query({ query: CURRENTUSER });
+    console.log(temp);
+    let temp_user;
+    let temp_loans = [];
+    if (temp.data.currentUser) {
       temp_user = {
         userType: temp.data.currentUser.userType,
-        email:  temp.data.currentUser.email,
+        email: temp.data.currentUser.email,
         firstName: temp.data.currentUser.firstName,
         lastName: temp.data.currentUser.lastName,
         address: temp.data.currentUser.address,
         personNumber: temp.data.currentUser.personNumber,
-        phone: temp.data.currentUser.phone
-      }
-      temp.data.currentUser.loans.map((obj,i) => {
-        if (obj.isActive === "true"){
+        phone: temp.data.currentUser.phone,
+      };
+      temp.data.currentUser.loans.map((obj, i) => {
+        if (obj.isActive === 'true') {
           temp_loans[i] = {
             id: obj.id,
-            loanDate: obj.loanDate !== null ? <Moment>{obj.loanDate}</Moment> : null,
-            returnDate: obj.returnDate !== null ? <Moment>{obj.returnDate}</Moment> : null,
-            dueDate: obj.dueDate !== null ? <Moment>{obj.dueDate}</Moment> : null,
+            loanDate:
+              obj.loanDate !== null ? <Moment>{obj.loanDate}</Moment> : null,
+            returnDate:
+              obj.returnDate !== null ? (
+                <Moment>{obj.returnDate}</Moment>
+              ) : null,
+            dueDate:
+              obj.dueDate !== null ? <Moment>{obj.dueDate}</Moment> : null,
             idCode: obj.deviceId.idCode,
             manufacture: obj.device.manufacture,
             model: obj.deviceId.model,
-            deviceType: obj.device.category.deviceType
-          }
+            deviceType: obj.device.category.deviceType,
+          };
         }
-      })
+      });
     }
-    this.setState({ data_user: temp_user })
-    this.setState({ data_loans: temp_loans })
+    this.setState({ data_user: temp_user });
+    this.setState({ data_loans: temp_loans });
   }
 
   render() {
     const { classes } = this.props;
-    const { 
-      data_user,
-      data_loans,
-      rows, 
-      columns,
-      sorting
-    } = this.state;
+    const { data_user, data_loans, rows, columns, sorting } = this.state;
     return (
       <Paper className={classes.root}>
         <h1>Summary</h1>
-        <Grid container spacing={16}
-              direction="row"
-              justify="space-evenly"
-              alignItems="center">
+        <Grid
+          container
+          spacing={16}
+          direction="row"
+          justify="space-evenly"
+          alignItems="center"
+        >
           <Grid item xs={11}>
             <Card className={classes.card} elevation={4}>
               <CardContent>
-                <Typography variant="h3"  gutterBottom>
+                <Typography variant="h3" gutterBottom>
                   Account information
                 </Typography>
-                <Typography variant="h5" component="h2">
-                  User type: {data_user.userType} {<br></br>}
-                  First name: {data_user.firstName} {<br></br>}
-                  Last name: {data_user.lastName} {<br></br>}
-                  Email: {data_user.email} {<br></br>}
-                  Address: {data_user.address} {<br></br>}        
-                  Phone: {data_user.phone} {<br></br>}
-                  Person number: {data_user.personNumber} 
-                </Typography>
+                <Grid container spacing={24}>
+                  <Grid item xs={6}>
+                    <Typography variant="h5" component="h2">
+                      First name: {data_user.firstName} {<br />}
+                      Last name: {data_user.lastName} {<br />}
+                      Address: {data_user.address} {<br />}
+                      Phone: {data_user.phone} {<br />}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="h5" component="h2">
+                      Email: {data_user.email} {<br />}
+                      Person number: {data_user.personNumber} {<br />}
+                      User type: {data_user.userType} {<br />}
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Typography variant="h5" component="h2" />
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs ={12}>
-              <h2>Your loans</h2>
+          <Grid item xs={12}>
+            <h2>Your loans</h2>
             <Paper className={classes.paperTable} elevation={6}>
-              <GridTable
-                rows={data_loans}
-                columns={columns}
-              >
+              <GridTable rows={data_loans} columns={columns}>
                 <SearchState />
-                <SortingState 
+                <SortingState
                   sorting={sorting}
                   onSortingChange={this.changeSorting}
                 />
@@ -168,14 +175,12 @@ class Home extends React.Component {
                 <Toolbar />
                 <SearchPanel />
               </GridTable>
-            </Paper>          
+            </Paper>
           </Grid>
         </Grid>
-
       </Paper>
-    )
-  };
+    );
+  }
 }
-
 
 export default withStyles(styles)(withApollo(Home));
