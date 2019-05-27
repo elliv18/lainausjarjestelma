a8dc8dda1669bf0d8befcdab0f49d9c9680765f0
Eevert Ahola <eevert.ahola@eng.tamk.fi>
2019-05-27 12:28:03 +0300

    jaa

--- frontend/pages/users.js
+++ frontend/pages/users.js
@@ -1,10 +1,9 @@
-import App from '../components/App'
-import MiniDrawer from '../components/MiniDrawer'
-import Print from '../components/Print'
-import Users from '../components/Users'
+import App from '../components/App';
+import MiniDrawer from '../components/MiniDrawer';
+import Users from '../components/Users';
 
-export default() => (
-    <MiniDrawer>
-        <Users/>
-    </MiniDrawer>
-)
+export default () => (
+  <MiniDrawer>
+    <Users />
+  </MiniDrawer>
+);
