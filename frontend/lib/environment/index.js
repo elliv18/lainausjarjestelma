a0953ddfb5c39e66a2187d8da2c3c57cb22cf34f
Toni Pakarinen <toni.pakarinen@eng.tamk.fi>
2019-06-05 16:05:38 +0300

    fixing

--- frontend/lib/environment/index.js
+++ frontend/lib/environment/index.js
@@ -7,7 +7,7 @@ export const IS_BROWSER = typeof window !== 'undefined';
 export const IS_SERVER = !IS_BROWSER;
 export const JWT = IS_BROWSER && localStorage.getItem('jwtToken');
 
-export const BACKEND_HOST =  || 'http://localhost';
+export const BACKEND_HOST = process.env.BACKEND_HOST || 'http://localhost';
 export const BACKEND_PORT = process.env.BACKEND_PORT || 3050;
 
 console.log(`
