659b0e54180006cceb82ed5f033a5113fb63212f
Toni Pakarinen <toni.pakarinen@eng.tamk.fi>
2019-06-14 11:06:35 +0300

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -47,7 +47,8 @@ const server = new GraphQLServer({
 // Server settings
 const options = {
   port: BACKEND_PORT,
-  playground: NODE_ENV === PRODUCTION ? "false" : "/",
+  playground: NODE_ENV === PRODUCTION ? "/" : "/",
+  //playground: NODE_ENV === PRODUCTION ? "false" : "/",
   cors: {
     creditials: false,
     origin: "*"
