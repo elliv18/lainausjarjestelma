f2c57001837036e4577f57cea7cd652b71104fbc
Ville <helminen12@gmail.com>
2019-06-18 15:53:18 +0300

    pwa update

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,8 +1,21 @@
 import App from '../components/App';
 import LoginTab from '../components/Login';
 
-export default () => (
-  <App>
-    <LoginTab />
-  </App>
-);
+export default class Index extends React.Component {
+  componentDidMount = () => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .catch(err => console.error('Service worker registration failed', err));
+    } else {
+      console.log('Service worker not supported');
+    }
+  };
+  render() {
+    return (
+      <App>
+        <LoginTab />
+      </App>
+    );
+  }
+}
