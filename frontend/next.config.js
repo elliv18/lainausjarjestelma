9d7523460d0c61e0d8bce03847b187a28807de22
Eevert Ahola <eevert.ahola@eng.tamk.fi>
2019-06-18 15:02:18 +0300

    hmm

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-const withOffline = require('next-offline');
-
 const nextConfig = {
   publicRuntimeConfig: {
     BACKEND_HOST: process.env.BACKEND_HOST,
@@ -9,26 +7,5 @@ const nextConfig = {
     PUBLIC_URL: process.env.PUBLIC_URL,
     PUBLIC_API_URL: process.env.PUBLIC_API_URL,
   },
-  workboxOpts: {
-    swDest: '/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
-  },
-  generateInDevMode: true,
 };
-module.exports = withOffline(nextConfig);
+module.exports = nextConfig;
