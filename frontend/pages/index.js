d3edbc7e1d8394281a9f2b5313192cfbede8470f
Ville <helminen12@gmail.com>
2019-06-19 10:07:02 +0300

    ydinpommi

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,21 @@
 import Home from '../components/Home';
 import MiniDrawer from '../components/MiniDrawer';
 
-export default () => (
-  <MiniDrawer>
-    <Home />
-  </MiniDrawer>
-);
+export default class Index extends React.Component {
+  componentDidMount = () => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .catch(err => console.error('Service worker registration failed', err));
+    } else {
+      console.log('Service worker not supported');
+    }
+  };
+  render() {
+    return (
+      <MiniDrawer>
+        <Home />
+      </MiniDrawer>
+    );
+  }
+}
--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,21 +1,17 @@
-/*
- *
- *  Air Horner
- *  Copyright 2015 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+);
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.routing.registerRoute(
+  new RegExp('.*.js'),
+  new workbox.strategies.NetworkFirst()
+);
 
 const version = '0.6.11';
 const cacheName = `v1`;
